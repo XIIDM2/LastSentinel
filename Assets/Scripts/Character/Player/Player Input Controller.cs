40ae17ba58d8c496c4c88b9dbae03ab065734e9b
XIIDM2 <XIIDM2@gmail.com>
2025-07-23 11:48:17 +0300

    Changed Player Input

--- Assets/Scripts/Character/Player/Player Input Controller.cs
+++ Assets/Scripts/Character/Player/Player Input Controller.cs
@@ -10,67 +10,117 @@ public class PlayerInputController : MonoBehaviour
     public event Action OnAttackPressed;
     public event Action OnAttackReleased;
 
-    private InputSystem_Actions inputActions;
-
+    private PlayerInput playerInput;
     private void Awake()
     {
-        inputActions = new InputSystem_Actions();
+        playerInput = GetComponent<PlayerInput>();
+
     }
 
     private void OnEnable()
     {
-        inputActions.Player.Move.performed += OnMovePerformed;
-        inputActions.Player.Move.canceled += OnMoveCanceled;
-
-        inputActions.Player.Jump.performed += OnJumpPerformed;
-        inputActions.Player.Jump.canceled += OnJumpCanceled;
-
-        inputActions.Player.Attack.performed += OnAttackPerformed;
-        inputActions.Player.Attack.canceled += OnAttackCanceled;
-
-        inputActions.Enable();
+        playerInput.onActionTriggered += HandleAction;
     }
 
     private void OnDisable()
     {
-        inputActions.Player.Move.performed -= OnMovePerformed;
-        inputActions.Player.Move.canceled -= OnMoveCanceled;
-
-        inputActions.Player.Jump.performed -= OnJumpPerformed;
-        inputActions.Player.Jump.canceled -= OnJumpCanceled;
-
-        inputActions.Player.Attack.performed -= OnAttackPerformed;
-        inputActions.Player.Attack.canceled -= OnAttackCanceled;
-
-        inputActions.Disable();
+        playerInput.onActionTriggered -= HandleAction;
     }
-    private void OnMovePerformed(InputAction.CallbackContext context)
+
+    private void HandleAction(InputAction.CallbackContext context)
     {
-        horizontalMovement?.Invoke(context.ReadValue<Vector2>().x);
+        switch (context.action.name)
+        {
+            case "Move":
+                horizontalMovement?.Invoke(context.ReadValue<Vector2>().x);
+                break;
+            case "Jump":
+                if (context.started)
+                {
+                    OnJumpPressed?.Invoke();
+                }
+                else if (context.canceled)
+                {
+                    OnJumpReleased?.Invoke();
+                }
+                break;
+            case "Attack":
+                if (context.started)
+                {
+                    OnAttackPressed?.Invoke();
+                }
+                else if (context.canceled)
+                {
+                    OnAttackReleased?.Invoke();
+                }
+                break;
+        }
     }
 
-    private void OnMoveCanceled(InputAction.CallbackContext context)
-    {
-        horizontalMovement?.Invoke(context.ReadValue<Vector2>().x);
-    }
+    #region C# InputSystemActions Script
+    //private InputSystem_Actions inputActions;
 
-    private void OnJumpPerformed(InputAction.CallbackContext context)
-    {
-        OnJumpPressed?.Invoke();
-    }
+    //private void Awake()
+    //{
+    //    inputActions = new InputSystem_Actions();
+    //}
 
-    private void OnJumpCanceled(InputAction.CallbackContext context)
-    {
-        OnJumpReleased?.Invoke();
-    }
+    //private void OnEnable()
+    //{
+    //    inputActions.Player.Move.performed += OnMovePerformed;
+    //    inputActions.Player.Move.canceled += OnMoveCanceled;
 
-    private void OnAttackPerformed(InputAction.CallbackContext context)
-    {
-        OnAttackPressed?.Invoke();
-    }
+    //    inputActions.Player.Jump.performed += OnJumpPerformed;
+    //    inputActions.Player.Jump.canceled += OnJumpCanceled;
 
-    private void OnAttackCanceled(InputAction.CallbackContext context)
-    {
-        OnAttackReleased?.Invoke();
-    }
+    //    inputActions.Player.Attack.performed += OnAttackPerformed;
+    //    inputActions.Player.Attack.canceled += OnAttackCanceled;
+
+    //    inputActions.Enable();
+
+    //}
+
+    //private void OnDisable()
+    //{
+    //    inputActions.Player.Move.performed -= OnMovePerformed;
+    //    inputActions.Player.Move.canceled -= OnMoveCanceled;
+
+    //    inputActions.Player.Jump.performed -= OnJumpPerformed;
+    //    inputActions.Player.Jump.canceled -= OnJumpCanceled;
+
+    //    inputActions.Player.Attack.performed -= OnAttackPerformed;
+    //    inputActions.Player.Attack.canceled -= OnAttackCanceled;
+
+    //    inputActions.Disable();
+    //}
+    //private void OnMovePerformed(InputAction.CallbackContext context)
+    //{
+    //    horizontalMovement?.Invoke(context.ReadValue<Vector2>().x);
+    //}
+
+    //private void OnMoveCanceled(InputAction.CallbackContext context)
+    //{
+    //    horizontalMovement?.Invoke(context.ReadValue<Vector2>().x);
+    //}
+
+    //private void OnJumpPerformed(InputAction.CallbackContext context)
+    //{
+    //    OnJumpPressed?.Invoke();
+    //}
+
+    //private void OnJumpCanceled(InputAction.CallbackContext context)
+    //{
+    //    OnJumpReleased?.Invoke();
+    //}
+
+    //private void OnAttackPerformed(InputAction.CallbackContext context)
+    //{
+    //    OnAttackPressed?.Invoke();
+    //}
+
+    //private void OnAttackCanceled(InputAction.CallbackContext context)
+    //{
+    //    OnAttackReleased?.Invoke();
+    //}
+    #endregion
 }
